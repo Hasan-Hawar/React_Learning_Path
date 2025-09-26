08a79162938a52c50fd5ee09577bdb61215a09f5
Hasan Hawar <hasan.hawar92@hotmail.com>
2025-09-26 22:42:34 +0200

    update state based on old state

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
       <div id="game-container">
         <ol id="players">
           <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="2" />
+          <Player name="Player 2" symbol="O" />
         </ol>
         GAME BOARD
       </div>
--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -3,17 +3,18 @@ export default function Player({ name, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
 
     const handleToggleEdit = () => {
-        setIsEditing(true);
+        setIsEditing(editing => !editing);
     }
     let playerName = <span className="player-name">{name}</span>
+    // let buttonCaption = "Save";
     if (isEditing) {
-        playerName = <input type="text" required />
+        playerName = <input type="text" required value={name} />
     }
     return <li>
         <span className="player">
             {playerName}
             <span className="player-sympol">{symbol}</span>
         </span>
-        <button onClick={handleToggleEdit}>Edit</button>
+        <button onClick={handleToggleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
 }
