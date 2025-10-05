b42d1251d89c07ee210eff8dfabc29b76961c9eb
Hasan Hawar <hasan.hawar92@hotmail.com>
2025-10-05 23:33:23 +0200

    improve code

--- 07-tic-tac-toe-starting-project/src/components/GameOver.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameOver.jsx
@@ -2,8 +2,7 @@ export default function GameOver({ winner }) {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
-      {winner && <p>Player {winner} won!</p>}
-      {!winner && <p>it's a drow!</p>}
+      {winner ? <p>Player {winner} wins!</p> : <p>it's a drow!</p>}
       <p>
         <button>Remactch!</button>
       </p>
